435f5e4fd3c9d5f4471a67a4f5fb30b73356a03d
servandserv <servandserv@gmail.com>
2019-12-01 04:21:52 -0500

    multiple connects

--- classes/com/servandserv/persistence/domain/service/pdo/ConnectionFactory.php
+++ classes/com/servandserv/persistence/domain/service/pdo/ConnectionFactory.php
@@ -4,10 +4,10 @@ namespace com\servandserv\persistence\domain\service\pdo;
 
 class ConnectionFactory
 {
-
     private static $instance;
-    private $pdo;
+    private $connections = [];
 
+    /**
     private function __construct( $dns, $user, $pass, $opt )
     {
         $opt = [
@@ -15,18 +15,41 @@ class ConnectionFactory
             \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
         ];
         try {
-            $this->pdo = new \PDO( $dns, $user, $pass, $opt );
+            $this->connections[] =  new \PDO( $dns, $user, $pass, $opt );
         } catch ( \PDOException $e ) {
             die( "Connection error: ".$e->getMessage() );
         }
     }
+    */
 
     public static function getConnect( $dns, $user, $pass, $opt )
     {
-        if( NULL == self::$instance ) {
-            self::$instance = new self( $dns, $user, $pass, $opt );
+        if( NULL === self::$instance ) {
+            self::$instance = new self();
         }
-        return self::$instance->pdo;
+        return self::$instance->createConnect(  $dns, $user, $pass, $opt );
+    }
+    
+    private function createConnect( $dns, $user, $pass, $opt )
+    {
+        $key = $this->connectionUID( $dns, $user );
+        if( !isset( $this->connections[$key] ) ) {
+            $opt = [
+                \PDO::ATTR_ERRMODE            => \PDO::ERRMODE_EXCEPTION,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            ];
+            try {
+                $this->connections[$key] =  new \PDO( $dns, $user, $pass, $opt );
+            } catch ( \PDOException $e ) {
+                die( "Connection error: ".$e->getMessage() );
+            }
+        }
+        return $this->connections[$key];
+    }
+    
+    private function connectionUID( $dns, $user ) 
+    {
+        return $key = md5( $dns.$user );
     }
 
 }
